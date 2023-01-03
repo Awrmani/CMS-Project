53e34cdafe82305d5757bcbd928d64a3d07a9c8f
Awrmani <85237160+Awrmani@users.noreply.github.com>
2023-01-02 20:46:43 -0500

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Arman Valaee Asr 2023</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
---
+++ index.php
@@ -0,0 +1,106 @@
+<?php include "includes/db.php" ?>
+<?php include "includes/header.php" ?>
+
+<!-- Navigation -->
+<?php include "includes/navigation.php" ?>
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+
+            <?php
+            $query = "SELECT * FROM posts";
+
+            $allPosts = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($allPosts)) {
+                $postTitle = $row['post_title'];
+                $postAuthor = $row['post_author'];
+                $postDate = $row['post_date'];
+                $postImage = $row['post_image'];
+                $postContent = $row['post_content'];
+            ?>
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php print($postTitle); ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php print($postAuthor); ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php print($postDate); ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php print($postImage); ?>" alt="PHP Logo">
+                <hr>
+                <p><?php print($postContent); ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+            <?php
+            }
+            ?>
+
+            <!-- Second Blog Post -->
+            <h2>
+                <a href="#">Blog Post Title</a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php">Start Bootstrap</a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+            <hr>
+            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+
+            <!-- Third Blog Post -->
+            <h2>
+                <a href="#">Blog Post Title</a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php">Start Bootstrap</a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+            <hr>
+            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="previous">
+                    <a href="#">&larr; Older</a>
+                </li>
+                <li class="next">
+                    <a href="#">Newer &rarr;</a>
+                </li>
+            </ul>
+
+        </div>
+
+        <?php include "includes/sidebar.php" ?>
+
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "includes/footer.php" ?>
+
+    </html>
